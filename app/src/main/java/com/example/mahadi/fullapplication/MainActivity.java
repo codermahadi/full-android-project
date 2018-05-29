7f15fcfd9edc124ed7767153b50141fd0e13a43a
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-05-29 14:08:12 +0600

    full drawer menu

--- app/src/main/java/com/example/mahadi/fullapplication/MainActivity.java
+++ app/src/main/java/com/example/mahadi/fullapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.mahadi.fullapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     private DrawerLayout myDrawerLayout;
@@ -22,15 +25,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initToolBar();
 
-        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        drawerToggle = new ActionBarDrawerToggle(this, myDrawerLayout, R.string.open, R.string.close);
+        myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, myDrawerLayout, toolbar, R.string.open, R.string.close);
         myDrawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+
+    //  function:Custom Tool bar.
     public void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.toolbarTitle);
@@ -45,18 +63,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (drawerToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//}
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-// Handle item selection
+        // Handle item selection
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.miCompose:
                 Toast.makeText(MainActivity.this, "clicking on email", Toast.LENGTH_SHORT).show();
@@ -68,5 +81,34 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.db) {
+            Toast.makeText(MainActivity.this, "clicking on Dashboard", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.even) {
+            Toast.makeText(MainActivity.this, "clicking on Even", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.email) {
+            Toast.makeText(MainActivity.this, "clicking on Email", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.blog) {
+            Toast.makeText(MainActivity.this, "clicking on Blog", Toast.LENGTH_SHORT).show();
+
+        }else if (id == R.id.activities) {
+            Toast.makeText(MainActivity.this, "clicking on activities", Toast.LENGTH_SHORT).show();
+
+        }else if (id == R.id.logout) {
+            Toast.makeText(MainActivity.this, "clicking on logout", Toast.LENGTH_SHORT).show();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
